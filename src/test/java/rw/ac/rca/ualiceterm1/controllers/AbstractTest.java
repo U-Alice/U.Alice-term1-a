93186fea880172b441946a661d709a627e75c8cf
U-Alice <umugwanezaalice22@gmail.com>
2023-12-18 10:57:47 +0200

    integration and unit test

--- src/test/java/rw/ac/rca/ualiceterm1/controllers/AbstractTest.java
+++ src/test/java/rw/ac/rca/ualiceterm1/controllers/AbstractTest.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
--- src/test/java/rw/ac/rca/ualiceterm1/controllers/MathControllerIntegrationTest.java
+++ src/test/java/rw/ac/rca/ualiceterm1/controllers/MathControllerIntegrationTest.java
@@ -1,28 +1,30 @@
 package rw.ac.rca.ualiceterm1.controllers;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 import rw.ac.rca.ualiceterm1.dtos.DoMathRequest;
 import rw.ac.rca.ualiceterm1.payload.ApiResponse;
+import rw.ac.rca.ualiceterm1.utils.TestUtil;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@WebMvcTest(MathController.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MathControllerIntegrationTest {
-    private static RestTemplate restTemplate;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void testSaveItem() {
+   public void testDoMath() {
         DoMathRequest dto = new DoMathRequest(1,2, "+");
-
-        ResponseEntity<ApiResponse> response = restTemplate.exchange("/do-math", HttpMethod.POST, dto, ApiResponse.class);
-
+        ResponseEntity<ApiResponse> response = restTemplate.postForEntity("/do-math",  TestUtil.createHttpEntity(dto), ApiResponse.class);
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
     }
 
